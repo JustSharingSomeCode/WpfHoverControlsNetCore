ee5b68a6f3a8501b6f7df6b9e0fccbfd029508e9
Oscar <oscarjuliandiazlopez@gmail.com>
2022-04-15 21:27:57 -0500

    Expander control

---
+++ WpfHoverControls/HoverExpander.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHoverControls
+{
+    /// <summary>
+    /// Realice los pasos 1a o 1b y luego 2 para usar este control personalizado en un archivo XAML.
+    ///
+    /// Paso 1a) Usar este control personalizado en un archivo XAML existente en el proyecto actual.
+    /// Agregue este atributo XmlNamespace al elemento raíz del archivo de marcado en el que 
+    /// se va a utilizar:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WpfHoverControls"
+    ///
+    ///
+    /// Paso 1b) Usar este control personalizado en un archivo XAML existente en otro proyecto.
+    /// Agregue este atributo XmlNamespace al elemento raíz del archivo de marcado en el que 
+    /// se va a utilizar:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WpfHoverControls;assembly=WpfHoverControls"
+    ///
+    /// Tendrá también que agregar una referencia de proyecto desde el proyecto en el que reside el archivo XAML
+    /// hasta este proyecto y recompilar para evitar errores de compilación:
+    ///
+    ///     Haga clic con el botón secundario del mouse en el proyecto de destino en el Explorador de soluciones y seleccione
+    ///     "Agregar referencia"->"Proyectos"->[Busque y seleccione este proyecto]
+    ///
+    ///
+    /// Paso 2)
+    /// Prosiga y utilice el control en el archivo XAML.
+    ///
+    ///     <MyNamespace:HoverExpander/>
+    ///
+    /// </summary>
+    public class HoverExpander : ContentControl
+    {
+        static HoverExpander()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HoverExpander), new FrameworkPropertyMetadata(typeof(HoverExpander)));
+        }
+
+
+
+        public bool IsExpanded
+        {
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsExpanded.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register("IsExpanded", typeof(bool), typeof(HoverExpander), new PropertyMetadata(false));
+
+        public override void OnApplyTemplate()
+        {
+            HoverButton btn = GetTemplateChild("ExpanderBtn") as HoverButton;
+            btn.Click += ExpanderBtn_Click;
+
+            base.OnApplyTemplate();
+        }
+
+        private void ExpanderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            IsExpanded = !IsExpanded;
+        }
+    }
+}
